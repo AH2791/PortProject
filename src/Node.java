30184afc2b6044da4483162c5a1429aa2190d045
AH2791 <hajjahmad20@gmail.com>
2022-11-28 21:57:47 +0200

    Fixed LL Node Missing

--- src/Node.java
+++ src/Node.java
@@ -1,10 +1,10 @@
 public class Node {
-    int key;     //data value.
-    Node parent; //points to the parent of the node.
-    Node left;   //points to the left child of the node.
-    Node right;  //points to the right child of the node.
-    int height;  //height of the node.
-
+    int key;
+    Node parent;
+    Node left;
+    Node right;
+    int height;
+    Node next;
     public Node(int data){
         this.key=data;
         this.parent=null;
