6fca33450b77013406e5d683a9787cc13310a9f9
MoetassemM <soumamikati.sm@gmail.com>
2022-11-28 21:45:40 +0200

    new classes

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -2,27 +2,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BinarySearchTree {
-    int size;  //number of elements in the Tree.
-    Node root; //root of the tree.
+    int size;
+    Node root;
 
     public BinarySearchTree(){
         this.size=0;
         this.root=null;
     }
-
-    //function to find specific data in the Tree. If found the return that node otherwise return the last reached node.
     public Node find(int data){
         Node temp = root;
         Node prev = null;
         if(temp==null){
             return temp;
         }
-
         while(temp!=null){
             if(temp.key==data){
                 return temp;
             }
-
             if(data>temp.key){
                 prev=temp;
                 temp=temp.right;
@@ -36,8 +32,6 @@ public class BinarySearchTree {
         }
         return prev;
     }
-
-    //function add elements in the Tree.
     public void insert(int data){
         System.out.println("Inserting data : " + data);
         Node node = new Node(data);
@@ -57,9 +51,6 @@ public class BinarySearchTree {
             parent.left=node;
         }
     }
-
-    //finds just the next big node than the given node.Works correctlly only when the given node is actually in the tree.
-    //if the given key is largest then it returns the same node;
     public Node findNext(int data){
         Node temp = find(data);
         if(temp.key!=data){
@@ -68,14 +59,12 @@ public class BinarySearchTree {
         }
         Node right = temp.right;
         Node parent = temp.parent;
-
         if(right!=null){
             while(right.left!=null){
                 right=right.left;
             }
             return right;
         }
-
         while(parent!=null){
             if(parent.key>temp.key){
                 return parent;
@@ -84,9 +73,6 @@ public class BinarySearchTree {
         }
         return temp;
     }
-
-
-    //return the numbers between data1 and data2, including data1 and data2 if they are present in the tree.
     public List<Node> rangeSearch(int data1, int data2){
         System.out.println("Range Search between " + data1 + " and " + data2);
         List<Node> list = new ArrayList<Node>();
@@ -94,7 +80,6 @@ public class BinarySearchTree {
         if(node1==null){
             return list;
         }
-
         while(node1.key<=data2){
             if(node1.key>=data1){
                 list.add(node1);
@@ -108,7 +93,6 @@ public class BinarySearchTree {
         return list;
     }
 
-    //function to remove elements from the tree.
     public void delete(int data){
         System.out.println("Remove  : " + data);
         Node temp = find(data);
@@ -139,7 +123,6 @@ public class BinarySearchTree {
                 return;
             }
         }
-
         Node nextTemp = findNext(temp.key);
         if(nextTemp.right!=null){
             nextTemp.right.parent=nextTemp.parent;
@@ -153,8 +136,6 @@ public class BinarySearchTree {
         if(nextTemp.parent!=null && nextTemp.parent.right==nextTemp){
             nextTemp.parent.right=nextTemp.right;
         }
-
-
         if(temp.left!=null){
             temp.left.parent=nextTemp;
         }
@@ -164,8 +145,6 @@ public class BinarySearchTree {
         nextTemp.left=temp.left;
         nextTemp.right=temp.right;
         nextTemp.parent=temp.parent;
-
-
         if(temp.parent==null){
             root=nextTemp;
             temp=null;
